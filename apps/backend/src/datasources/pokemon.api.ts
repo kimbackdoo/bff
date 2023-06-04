7bb7339b9e9cb88d1ae574c5159f69cb31203aff
kimbackdoo <kdk7121743@naver.com>
2023-06-04 12:41:16 +0900

    fix: generateQueryParams 함수 적용

--- apps/backend/src/datasources/pokemon.api.ts
+++ apps/backend/src/datasources/pokemon.api.ts
@@ -1,15 +1,16 @@
 import { NamedApiResourceList, Pokemon, QueryPokemonsArgs } from '@/__generated__/types'
+import { generateQueryParams } from '@/utils/generate'
 import { RESTDataSource } from '@apollo/datasource-rest'
 
 export default class PokemonAPI extends RESTDataSource {
     override baseURL = process.env.POKEMON_BASE_API_URL
 
     async getPokemons(params: Partial<QueryPokemonsArgs>): Promise<NamedApiResourceList> {
-        const queryParams = { limit: params?.limit?.toString(), offset: params?.offset?.toString() }
+        const queryParams = generateQueryParams(params)
         return this.get('pokemon', { params: queryParams })
     }
 
     async getPokemon(id: number): Promise<Pokemon> {
-        return this.get(`pokemon/${id}`)
+        return this.get(`pokemon/${encodeURIComponent(id)}`)
     }
 }
