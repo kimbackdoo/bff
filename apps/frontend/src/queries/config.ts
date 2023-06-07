5fb825a5f525e0f4b611280f1ce52e0633ec095e
kimbackdoo <kdk7121743@naver.com>
2023-06-07 17:57:52 +0900

    feat: graphql + react query custom hook 추가

---
+++ apps/frontend/src/queries/config.ts
@@ -0,0 +1,42 @@
+import { type TypedDocumentNode } from '@graphql-typed-document-node/core'
+import {
+    InfiniteData,
+    UseInfiniteQueryOptions,
+    UseInfiniteQueryResult,
+    UseQueryOptions,
+    UseQueryResult,
+    useInfiniteQuery,
+    useQuery,
+} from '@tanstack/react-query'
+import request from 'graphql-request'
+
+type UseGraphQLResult<TResult> = Omit<UseQueryResult<TResult>, 'data'> & { data: TResult }
+type UseInfiniteGraphQLResult<TResult> = Omit<UseInfiniteQueryResult<TResult>, 'data'> & { data: InfiniteData<TResult> }
+
+const ENDPOINT = 'http://localhost:4000'
+
+export function useGraphQL<TResult, TVariables>(
+    document: TypedDocumentNode<TResult, TVariables>,
+    options?: UseQueryOptions<TResult> & { variables?: TVariables }
+): UseGraphQLResult<TResult> {
+    const { variables } = options ?? {}
+    const { data, ...rest } = useQuery({
+        queryKey: [(document.definitions[0] as any).name.value, options?.variables],
+        queryFn: async () => request(ENDPOINT, document, variables ?? {}),
+        ...options,
+    })
+    return { data: data!, ...rest }
+}
+
+export function useInfiniteGraphQL<TResult, TVariables>(
+    document: TypedDocumentNode<TResult, TVariables>,
+    options?: UseInfiniteQueryOptions<TResult> & { variables?: TVariables }
+): UseInfiniteGraphQLResult<TResult> {
+    const { variables } = options ?? {}
+    const { data, ...rest } = useInfiniteQuery({
+        queryKey: [(document.definitions[0] as any).name.value, variables],
+        queryFn: async ({ pageParam }) => request(ENDPOINT, document, { ...(pageParam ?? variables) }),
+        ...options,
+    })
+    return { data: data!, ...rest }
+}
